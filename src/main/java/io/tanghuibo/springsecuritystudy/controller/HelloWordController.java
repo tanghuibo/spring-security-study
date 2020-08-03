a679c6a40e3918251de65b2915d0d1c7bcb59755
tang_huibo <1215893264@qq.com>
2020-08-04 00:28:43 +0800

    basic init

---
+++ src/main/java/io/tanghuibo/springsecuritystudy/controller/HelloWordController.java
@@ -0,0 +1,20 @@
+package io.tanghuibo.springsecuritystudy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tanghuibo
+ * @date 2020/8/3下午11:34
+ */
+@RequestMapping("helloWord")
+@RestController
+public class HelloWordController {
+
+    @GetMapping("sayHello")
+    public String sayHello(@RequestParam("name") String name) {
+        return String.format("hello %s", name);
+    }
+}
