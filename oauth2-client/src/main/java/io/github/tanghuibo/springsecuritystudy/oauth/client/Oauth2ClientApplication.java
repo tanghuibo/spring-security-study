6a53c0afdab1958e9cfeef7d1fc49d5f3c2b7816
tang_huibo <1215893264@qq.com>
2020-08-19 23:17:16 +0800

    搭建 Oauth2ClientApplication.java Oauth2ServiceApplication.java

---
+++ oauth2-client/src/main/java/io/github/tanghuibo/springsecuritystudy/oauth/client/Oauth2ClientApplication.java
@@ -0,0 +1,16 @@
+package io.github.tanghuibo.springsecuritystudy.oauth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tanghuibo
+ * @date 2020/8/19下午11:13
+ */
+@SpringBootApplication
+public class Oauth2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ClientApplication.class, args);
+    }
+}
---
+++ oauth2-service/src/main/java/io/github/tanghuibo/springsecuritystudy/oauth/service/Oauth2ServiceApplication.java
@@ -0,0 +1,16 @@
+package io.github.tanghuibo.springsecuritystudy.oauth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tanghuibo
+ * @date 2020/8/19下午11:13
+ */
+@SpringBootApplication
+public class Oauth2ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ServiceApplication.class, args);
+    }
+}
