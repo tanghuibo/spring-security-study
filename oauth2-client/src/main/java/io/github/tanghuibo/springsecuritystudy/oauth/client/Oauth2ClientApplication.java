37876da9108bab66f12b4515f04bcddc3c9822ab
tang_huibo <1215893264@qq.com>
2020-09-01 00:08:26 +0800

    设置context-path, 不然sso有问题

--- oauth2-client/src/main/java/io/github/tanghuibo/springsecuritystudy/oauth/client/Oauth2ClientApplication.java
+++ oauth2-client/src/main/java/io/github/tanghuibo/springsecuritystudy/oauth/client/Oauth2ClientApplication.java
@@ -3,7 +3,6 @@ package io.github.tanghuibo.springsecuritystudy.oauth.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @author tanghuibo
